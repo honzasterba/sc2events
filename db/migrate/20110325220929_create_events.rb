3de5f0699693fae5d505202712a74177cf9b8721
Honza Sterba <info@jansterba.com>
2011-03-25 23:11:27 +0100

    events migration

--- db/migrate/20110325220929_create_events.rb
+++ db/migrate/20110325220929_create_events.rb
@@ -1,7 +1,10 @@
 class CreateEvents < ActiveRecord::Migration
   def self.up
     create_table :events do |t|
-
+      t.string :name, :null => false
+      t.date :starts_at, :null => false
+      t.date :ends_at
+      t.string :homepage
       t.timestamps
     end
   end
