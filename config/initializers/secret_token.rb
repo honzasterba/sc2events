9a982e5c6a391b4a00a4fd7b55b41cb14afbfb77
Honza Sterba <info@jansterba.com>
2011-03-25 22:51:16 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sc2events::Application.config.secret_token = 'e83ff456d5d208213efee41875d2394864177f552eda5e57169db79fe778531ded607b6a156d0fb6f6e4b5e0c543a4c34a7912a00e81942da287e4e9de2cf494'
