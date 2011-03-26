7a78c66cff0685cdba6ab9009f8c26d0e2fc5ed3
Honza Sterba <info@jansterba.com>
2011-03-26 11:36:59 +0100

    fixing omniauth stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    auth = request.env['rack.auth']
+    auth = request.env['omniauth.auth']
     unless @auth = Authorization.find_from_hash(auth)
       # Create a new user or add an auth to existing user, depending on
       # whether there is already a user signed in.
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'Gu7ak0g5CIKGGF2jaRcKg', 'YWamg6YRBzsSxNAQ9jWAYiSh6ziBYjzmcvzPCLDQ'
+  provider :twitter, 'qlvIKFWOWzLweqWDpEFi1Q', 'OgWaraPr8hmOOEQFrkG7stlHp1Q5OfOxF4TfYOtw'
 #  provider :facebook, 'APP_ID', 'APP_SECRET'
 end
