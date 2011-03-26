2cc4916f49ae4d483d66f57ee72e038cfcc2c706
Honza Sterba <info@jansterba.com>
2011-03-26 01:50:31 +0100

    login logout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Sc2events::Application.routes.draw do
 
   match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/login', :to => 'sessions#new'
+  match '/logout', :to => 'sessions#destroy'
 
   resources :sessions, :only => [ :create ]
 
