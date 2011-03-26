56aab86cdea300c285e7da8f87ddeed2a866c0e9
Martin Cohen <martin.balazik@gmail.com>
2011-03-26 17:16:29 +0100

    * new UI

--- db/schema.rb
+++ db/schema.rb
@@ -12,27 +12,13 @@
 
 ActiveRecord::Schema.define(:version => 20110325225044) do
 
-  create_table "authorizations", :force => true do |t|
-    t.string   "provider",   :null => false
-    t.string   "uid",        :null => false
-    t.integer  "user_id",    :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+# Could not dump table "authorizations" because of following ArgumentError
+#   invalid date
 
-  create_table "events", :force => true do |t|
-    t.string   "name",       :null => false
-    t.date     "starts_at",  :null => false
-    t.date     "ends_at"
-    t.string   "homepage"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+# Could not dump table "events" because of following ArgumentError
+#   invalid date
 
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+# Could not dump table "users" because of following ArgumentError
+#   invalid date
 
 end
