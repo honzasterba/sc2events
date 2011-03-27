2d5d8108b3155ee5d32a299007352925f2efac90
Martin Cohen <martin.balazik@gmail.com>
2011-03-27 12:14:53 +0200

    * started changes in javascripts

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -5,12 +5,6 @@
 		{
 			var this_events = this;
 			
-			var starred = this.getStarred();
-			for( var i = 0; i < starred.length; i++ )
-			{
-				$('span.star.objid-event-'+starred[i]).removeClass('off').addClass('on');
-			}
-			
 			$(document).delegate( 'span.star', 'click', function( e )
 			{
 				e.preventDefault();
@@ -54,73 +48,17 @@
 				}
 			});
 			
-			this.updateStarred();
-			// window.localStorage.clear();
+			// ToDo: Update starred items?
 		},
 		
 		onStarred: function( id, is_starred )
 		{
-			if( is_starred )
-				this.addStarred( id );
-			else
-				this.removeStarred( id );
-		},
-		
-		getStarred: function()
-		{
-			var starred = window.localStorage.getItem('starred');
-			return starred ? starred.split(',') : [];
-		},
-		
-		setStarred: function( new_starred )
-		{
-			window.localStorage.setItem('starred', new_starred.join(','));
-			this.updateStarred();
-		},
-		
-		addStarred: function( id )
-		{
-			var starred = this.getStarred();
-			for( var i = 0; i < starred.length; i++ )
+			console.log( window.config );
+			var url = window.config.url +"/events/star/"+ ( is_starred ? 'on' : 'off' ) +'/'+id;
+			$.get( url, null, function( data )
 			{
-				if( starred[i] == id )
-				{
-					return false;
-				}
-			}
-			starred.push(parseInt(id));
-			this.setStarred( starred );
-			return true;
-		},
-		
-		removeStarred: function(id)
-		{
-			var starred = this.getStarred();
-			var new_starred = [];
-			for( var i = 0; i < starred.length; i++ )
-			{
-				if( starred[i] != id )
-				{
-					new_starred.push(starred[i]);
-				}
-			}
-			
-			if( starred.length == new_starred.length )
-			{
-				return false;
-			}
-			
-			this.setStarred(new_starred);
-			return true;
-		},
-		
-		// Sends currently starred items to the server and retrieves HTML for starred items.
-		updateStarred: function()
-		{
-			$.post( window.config.url+'/starred', {starred:this.getStarred()}, function( data )
-			{
-				$('#starred-list').html(data);
+				$("#starred-list").html( data );
+				$("#starred-list .new").fadeIn();
 			});
 		}
-		
 	}
