706fd55d6f4f8ec9c4de30e848c99b3bf076da7c
Honza Sterba <info@jansterba.com>
2011-03-27 23:16:02 +0200

    facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'qlvIKFWOWzLweqWDpEFi1Q', 'OgWaraPr8hmOOEQFrkG7stlHp1Q5OfOxF4TfYOtw'
-#  provider :facebook, 'APP_ID', 'APP_SECRET'
+  provider :facebook, '138028809601345', 'c5ea75b4165023c1bcba913abadeac71'
 end
