d168d8c098bab75b97de25bf7cdc4eadddf826a4
Honza Sterba <info@jansterba.com>
2011-04-07 14:28:21 +0200

    last attempt to get twiter working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ require 'omniauth/strategies/open_id'
 require 'omniauth/strategies/google_apps'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'iGs1H4w3EILBoD5X0gcw', 'hP88WIo8cfg9kzF68J2qahLrlwYMl9USlrAtuCK2Gg'
+  provider :twitter, 'tImv9eyaYOFRUSNpEjYMg', 'OLAoubaWuPrrVLoOuuBn2Nikjkp0QdQbbQAyxny4Svg'
   provider :facebook, '138028809601345', 'c5ea75b4165023c1bcba913abadeac71'
   provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => 'gmail.com'
   provider :open_id, OpenID::Store::Filesystem.new('/tmp')
